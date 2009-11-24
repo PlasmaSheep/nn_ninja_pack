3b221a17adc00d4c401c5b8fa4d3d21ec79726cd
Plasmasheep <alyosha.be@gmail.com>
2009-11-23 20:08:38 -0800

    Packaging script now also packages 'additions/', also outdated pk3 has been removed.

--- buildpack.sh
+++ buildpack.sh
@@ -3,12 +3,13 @@
 # Title: Packages the ninja pack
 # Created By: Tyler "-z-" Mulligan of www.nexuizninjaz.com
 #
-# Usage: place script in the same folder as your ninja pack root
+# Usage: place script in the same folder as your ninja pack root, and exec when you want to build.
 #
 
-# This Packages this experiment
+# This Packages this experiment.
+#It also packs the 'additions' folder now. --Plasma
 build_package() {
-	find $(pwd) -type f -print | egrep -v '\.git|additions|buildpack\.sh|nn_ninja_pack_|packlist.txt' | sed "s#$(pwd)/##" > packlist.txt
+	find $(pwd) -type f -print | egrep -v '\.git|buildpack\.sh|nn_ninja_pack_|packlist.txt' | sed "s#$(pwd)/##" > packlist.txt
 	xargs zip -r nn_ninja_pack_$( date +%m%d%y ).pk3 < packlist.txt
 	rm packlist.txt
 } # End pack_nst
